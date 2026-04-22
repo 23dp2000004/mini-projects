d82833374d5fed992cebb9a6a4f8e9cb3d9db662
Roshni SR <roshniqrs@gmail.com>
2026-04-22 21:23:28 +0530

    updated JS file in to-do-list-tracker

--- to-do-list-tracker/script.js
+++ to-do-list-tracker/script.js
@@ -16,9 +16,103 @@ button.addEventListener("click", function() {
         return; 
     }
 
-    // CREATE the new list item
-    const li = document.createElement("li");
-    li.textContent = taskValue;
+    // Create elements 
+
+    // list elements
+    const li = document.createElement("li"); 
+
+    const span = document.createElement("span"); 
+    span.textContent = taskValue; 
+
+    // done button
+    const doneBttn = document.createElement("button"); 
+    doneBttn.textContent = "Done"; 
+
+    // Add click event for done button
+        doneBttn.addEventListener("click", function() {
+
+        const doneList = document.getElementById("done-list");
+
+        // get the text from span
+        const text = span.textContent;
+
+        // create new li (text only)
+        const newLi = document.createElement("li");
+        newLi.textContent = text;
+
+        // add to done list
+        doneList.appendChild(newLi);
+
+        // remove original task
+        li.remove();
+    });
+
+    // discard button
+    const discardBttn = document.createElement("button"); 
+    discardBttn.textContent = "Discard"; 
+
+    // Add click event for discard button
+        discardBttn.addEventListener("click", function() {
+
+        const discardList = document.getElementById("discarded-list");
+
+        // get the text from span
+        const text = span.textContent;
+
+        // create new li (text only)
+        const newLi = document.createElement("li");
+        newLi.textContent = text;
+
+        // add to done list
+        discardList.appendChild(newLi);
+
+        // remove original task
+        li.remove();
+    });
+
+    
+
+    // edit button
+    const editBttn = document.createElement("button"); 
+    editBttn.textContent = "Edit";
+
+    // Add click event for Edit Button
+    editBttn.addEventListener("click", function() {
+
+        const currentText = span.textContent;
+
+        const newText = prompt("Edit your task:", currentText);
+
+        // if user cancels → do nothing
+        if (newText === null) {
+            return;
+        }
+
+        // if empty → also do nothing (optional rule)
+        if (newText === "") {
+            return;
+        }
+
+        // update text
+        span.textContent = newText;
+    });
+
+    // remove button
+    const removeBttn = document.createElement("button");
+    removeBttn.textContent = "Remove";
+
+    // Add click event for remove Button
+    removeBttn.addEventListener("click", function() {
+        li.remove();
+    });
+
+    // Append elements into li 
+    li.appendChild(span); 
+    li.appendChild(editBttn);
+    li.appendChild(doneBttn); 
+    li.appendChild(discardBttn); 
+    li.appendChild(removeBttn); 
+    
 
     // DISPLAY it in the To-Do list
     const todoList = document.getElementById("todo-list");
