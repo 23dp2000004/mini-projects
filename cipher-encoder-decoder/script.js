ae39a12026a6f520888ed7b9ce55f97e6036fb8d
Roshni SR <roshniqrs@gmail.com>
2026-04-24 08:00:42 +0530

    updated code for cipher-encoder-decoder to have working decoder for a1z26

--- cipher-encoder-decoder/script.js
+++ cipher-encoder-decoder/script.js
@@ -1,17 +1,18 @@
 // get the encode button
-const button = document.getElementById("encodeBttn");
+const buttonE = document.getElementById("encodeBttn");
 
 // add click event
-button.addEventListener("click", function(){
+buttonE.addEventListener("click", function(){
 
     const cipher = document.getElementById("cipherType").value;
+    //console.log(cipher)
 
 
     //take the input string and keep it here
     const mesg = document.getElementById("input").value;
     const message = mesg.toUpperCase();
     
-    //console.log(message)
+    //console.log(mesg)
 
 
     if(cipher === "a1z26"){
@@ -21,16 +22,23 @@ button.addEventListener("click", function(){
         // map alphabet to ascii then subtract 64 (use abs value)
         for (let i = 0; i < message.length; i++) {
             let ch = message[i];
-            let code = ch.charCodeAt(0) - 64;
 
-            //handle " " spaces later
-            // handle . , ? ! special symbols later
 
-            encryptedMesg =  encryptedMesg + " " + code;
-            //console.log(ch, code);
-            //console.log(encryptedMesg)
+            if (ch >= 'A' && ch <= 'Z'){
 
-            
+                let codeNum = ch.charCodeAt(0) - 64;
+                let fcode = codeNum.toString().padStart(2, "0");
+
+                // handle . , ? ! special symbols later
+
+                encryptedMesg += fcode;
+                //console.log(ch, code);
+                //console.log(encryptedMesg)
+
+            }
+            else{
+                encryptedMesg += ch;
+            }
         }
 
         const resultElement = document.getElementById("encryptedMesg");
@@ -39,7 +47,59 @@ button.addEventListener("click", function(){
     }
 
 
-    
-
-
 });
+
+
+// get decoder button
+const buttonD = document.getElementById("decodeBttn")
+
+// add click event
+buttonD.addEventListener("click", function(){
+
+    const cipher = document.getElementById("decipherType").value;
+    //console.log(cipher);
+
+    //take the input string and keep it here
+    const mesg = document.getElementById("encryptedInput").value;
+    //console.log(mesg);
+
+    if(cipher === "a1z26"){
+
+        let decryptedMesg = "";
+
+        // map number to alphabet and take care of padded 0s
+        for (let i = 0; i < mesg.length; i++) {
+            let ch = mesg[i];
+
+            if(ch >= '0' && ch <= '9'){
+
+                let pair = mesg[i] + mesg[i+1]; //string form: "01"
+                let num = Number(pair); //number: 1
+
+                if (num >= 1 && num <= 26){
+
+                    let codeNum = num + 64; //now we have ascii value
+                    let alphabet = String.fromCharCode(codeNum);
+
+                    decryptedMesg += alphabet;
+                    //console.log(decryptedMesg)
+
+                    i++
+                }
+            }   
+            else{
+                decryptedMesg += ch;
+            }
+
+
+        }
+
+        const resultElement = document.getElementById("decodedMesg");
+            resultElement.textContent = decryptedMesg;
+
+    }
+
+
+
+
+})
